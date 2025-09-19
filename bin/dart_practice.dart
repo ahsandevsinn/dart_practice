9bf897c70e26564bc57c7039c93ccda3922d3e7c
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-09-19 05:35:36 +0500

    comments

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,5 +1,7 @@
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_practice.calculate()}!');
+  print('Hello World Dart');
+    print(arguments);        // prints the list of arguments
+  print(arguments.length); 
 }
