1eb2fe85cc996716ccfe64edfee1cde6f4226d10
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-09-23 02:46:47 +0500

    final and const

--- lib/variables/variables.dart
+++ lib/variables/variables.dart
@@ -1,12 +1,22 @@
 class Variables {
   void display(){
-    int age = 10;
-    double d = 34.0;
-    bool isMarried = true;
-    String name = "Ahsan";
-    print(age);
-    print(d);
-    print(isMarried);
-    print(name);
+    // int age = 10;
+    // double d = 34.0;
+    // bool isMarried = true;
+    // String name = "Ahsan";
+    // print(age);
+    // print(d);
+    // print(isMarried);
+    // print(name);
+
+    // dynamic age = 34;
+    // age = 37.5;
+    // age = "43";
+    // print(age);
+
+    final name = "Ahsan";print(name);
+    const pi = 3.14;
+    print(pi);
+    
   }
 }
