7825c90c709d85afaed758de1f09177a552e0660
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-09-23 02:26:04 +0500

    static variables

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,7 +1,9 @@
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
+import 'package:dart_practice/variables/variables.dart';
 
 void main(List<String> arguments) {
   print('Hello World Dart');
     print(arguments);        // prints the list of arguments
   print(arguments.length); 
+  Variables().display();
 }
---
+++ lib/variables/variables.dart
@@ -0,0 +1,12 @@
+class Variables {
+  void display(){
+    int age = 10;
+    double d = 34.0;
+    bool isMarried = true;
+    String name = "Ahsan";
+    print(age);
+    print(d);
+    print(isMarried);
+    print(name);
+  }
+}
