d1f9baca2ef4f0853adeda680317bfec0b630038
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-05 12:38:53 +0500

    variables

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -2,6 +2,6 @@ import 'package:dart_practice/dart_practice.dart' as dart_practice;
 import 'package:dart_practice/variables/variables.dart';
 
 void main(List<String> arguments) {
-  print('Hello World Dart');
+
   Variables().display();
 }
--- lib/variables/variables.dart
+++ lib/variables/variables.dart
@@ -1,6 +1,19 @@
 class Variables {
   void display() {
     print(StaticCar.wheels);
+    dynamic age = 30;
+    print(age);
+    age = "Ahsan";
+    print(age);
+    final dateTIme = DateTime.now();
+    print(dateTIme);
+    const dateObj = "Oct 5";
+    print(dateObj);
+    var ageNu = 39;
+    print(ageNu);
+    // ageNu =  "Ahsan";
+    // ageNu = 3.9;
+
   }
 }
 
