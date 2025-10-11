6907de4c8983d52cade48c2bea095ee06047a1ba
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-11 17:21:58 +0500

    Create a simple calculator that prints the sum, difference, product, and quotient of two numbers.

--- lib/practice/practice_one.dart
+++ lib/practice/practice_one.dart
@@ -3,9 +3,21 @@ class PracticeOne {
   // Write a program to print “Hello, Dart!” on the screen.
   // print("Hello, Dart!");
   // Print your name, age, and country using print() statements.
-  print("Ahsan Khan");
-  print("20");
-  print("Pakistan");
+  // print("Ahsan Khan");
+  // print("20");
+  // print("Pakistan");
+
+// Create a simple calculator that prints the sum, difference, product, and quotient of two numbers.
+int a = 10;
+int b = 10;
+int sum = a+b;
+print(sum);
+int diff = a-b;
+print(diff);
+int product = a*b;
+print(product);
+int modules = a%b;
+print(product);
 
   }
   
