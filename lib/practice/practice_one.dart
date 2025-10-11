a23072feb9fdd19645431b67d318e820fcaa851d
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-11 16:53:34 +0500

    Print your name, age, and country using print() statements.

--- lib/practice/practice_one.dart
+++ lib/practice/practice_one.dart
@@ -1,7 +1,11 @@
 class PracticeOne {
  static practiceOne(){
   // Write a program to print “Hello, Dart!” on the screen.
-  print("Hello, Dart!");
+  // print("Hello, Dart!");
+  // Print your name, age, and country using print() statements.
+  print("Ahsan Khan");
+  print("20");
+  print("Pakistan");
 
   }
   
