16c1ad4556b8d7d2369b521ddab40c45aef75265
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-11 18:12:20 +0500

    Use string interpolation to display your name and favorite hobby in a sentence.

--- lib/practice/practice_one.dart
+++ lib/practice/practice_one.dart
@@ -8,16 +8,20 @@ class PracticeOne {
   // print("Pakistan");
 
 // Create a simple calculator that prints the sum, difference, product, and quotient of two numbers.
-int a = 10;
-int b = 10;
-int sum = a+b;
-print(sum);
-int diff = a-b;
-print(diff);
-int product = a*b;
-print(product);
-int modules = a%b;
-print(product);
+// int a = 10;
+// int b = 10;
+// int sum = a+b;
+// print(sum);
+// int diff = a-b;
+// print(diff);
+// int product = a*b;
+// print(product);
+// int modules = a%b;
+// print(product);
+// Use string interpolation to display your name and favorite hobby in a sentence.
+String name = "Ahsan Khan";
+String hobby = "Books Reading";
+print("My name is $name and $hobby favorite hobby in a sentence "  );
 
   }
   
