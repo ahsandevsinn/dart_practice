1fe6b73f8154ea4728b93c82d56c1cef8696d161
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-16 02:04:49 +0500

    Relational Operators

--- lib/Operators/operators.dart
+++ lib/Operators/operators.dart
@@ -1,15 +1,26 @@
 class Operators {
   static operators(){
     // Arthimetic Operator
-    int a = 10;
-    int b = 10;
-    print(a+b);
-    print(a-b);
-    print(a*b);
-    print(a~/b);
-    print(a/b);
-    print(a%b);
-    print(-(a+b));
+    // int a = 10;
+    // int b = 10;
+    // print(a+b);
+    // print(a-b);
+    // print(a*b);
+    // print(a~/b);
+    // print(a/b);
+    // print(a%b);
+    // print(-(a+b));
+
+    // Relational Operators
+    int c = 3;
+    int d= 6;
+    print(c>d);
+    print(c<d);
+    print(c>=d);
+    print(c<=d);
+    print(c==d);
+    print(c!=d);
+
   }
   
 }
