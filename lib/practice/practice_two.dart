8c426b540a7cc0c5f85c3d6436e541375a19baf4
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-21 09:51:38 +0500

    Compare two ages using relational operators

--- lib/practice/practice_two.dart
+++ lib/practice/practice_two.dart
@@ -2,37 +2,48 @@ class PracticeTwo {
   static practiceTwo() {
     // print("Hello Two");
     // Add two integers and print the result.
-    int a = 10;
-    int b = 10;
-    // print(a+b);
-    // Subtract one number from another.
-    print(a - b);
-    // Multiply two double values.
-    print(a * b);
-    // Divide two numbers and print the quotient.
-    print(a / b);
-    // Use integer division (~/) to find how many times 7 fits into 22.
-    int c = 7;
-    int d = 22;
-    print(d ~/ c);
-    // Find the remainder using %.
-    print(a % b);
-    // Check if a number is greater than another using >.
-    print(a > b);
-// Use == to compare two strings (“dart” and “flutter”).
-    print("dart" == "flutter");
+    // int a = 10;
+    // int b = 10;
+    // // print(a+b);
+    // // Subtract one number from another.
+    // print(a - b);
+    // // Multiply two double values.
+    // print(a * b);
+    // // Divide two numbers and print the quotient.
+    // print(a / b);
+//     // Use integer division (~/) to find how many times 7 fits into 22.
+//     int c = 7;
+//     int d = 22;
+//     print(d ~/ c);
+//     // Find the remainder using %.
+//     print(a % b);
+//     // Check if a number is greater than another using >.
+//     print(a > b);
+// // Use == to compare two strings (“dart” and “flutter”).
+//     print("dart" == "flutter");
 // Use a logical && to check if both conditions are true.
 // if (a == 10 && b == 10) {
 //   print("Value Matched")
 // ;}
 
 // Use a ternary (?:) operator to print “Even” or “Odd”.
-    a % 2 == 0 ? print("Even") : print("odd");
-    // Use assignment operators (+=, -=) to update a variable’s value.
-    int ab =4;
-    ab += ab;
-    print(ab);
-    ab -= ab;
-    print(ab);
+    // a % 2 == 0 ? print("Even") : print("odd");
+    // // Use assignment operators (+=, -=) to update a variable’s value.
+    // int ab =4;
+    // ab += ab;
+    // print(ab);
+    // ab -= ab;
+    // print(ab);
+    // Use ?? to assign a default value if a variable is null.
+    // int? a;
+    // print(a ?? 4);
+
+
+// Compare two ages using relational operators (>, <, ==).
+int a = 34;
+int b = 2;
+print(a == b);
+print(a>b);
+print(a<b);
   }
 }
