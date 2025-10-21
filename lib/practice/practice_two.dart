965dc8d16cff06e4e930b60fa5b2b1d3ebb1ac83
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-21 09:36:43 +0500

    Use assignment operators (+=, -=) to update a variable’s value.

--- lib/practice/practice_two.dart
+++ lib/practice/practice_two.dart
@@ -1,35 +1,38 @@
 class PracticeTwo {
- static practiceTwo(){
-  // print("Hello Two");
-  // Add two integers and print the result.
-  int a = 10;
-  int b = 10;
-  // print(a+b);
-  // Subtract one number from another.
-  print(a-b);
-  // Multiply two double values.
-  print(a*b);
-  // Divide two numbers and print the quotient.
-  print(a/b);
-  // Use integer division (~/) to find how many times 7 fits into 22.
-  int c = 7;
-  int d = 22;
-  print(d~/c);
-  // Find the remainder using %.
-  print(a%b);
-  // Check if a number is greater than another using >.
-  print(a>b);
+  static practiceTwo() {
+    // print("Hello Two");
+    // Add two integers and print the result.
+    int a = 10;
+    int b = 10;
+    // print(a+b);
+    // Subtract one number from another.
+    print(a - b);
+    // Multiply two double values.
+    print(a * b);
+    // Divide two numbers and print the quotient.
+    print(a / b);
+    // Use integer division (~/) to find how many times 7 fits into 22.
+    int c = 7;
+    int d = 22;
+    print(d ~/ c);
+    // Find the remainder using %.
+    print(a % b);
+    // Check if a number is greater than another using >.
+    print(a > b);
 // Use == to compare two strings (“dart” and “flutter”).
-print("dart" == "flutter");
+    print("dart" == "flutter");
 // Use a logical && to check if both conditions are true.
 // if (a == 10 && b == 10) {
 //   print("Value Matched")
 // ;}
 
-
 // Use a ternary (?:) operator to print “Even” or “Odd”.
-a%2==0 ?print("Even"):print("odd");
-
-
- } 
+    a % 2 == 0 ? print("Even") : print("odd");
+    // Use assignment operators (+=, -=) to update a variable’s value.
+    int ab =4;
+    ab += ab;
+    print(ab);
+    ab -= ab;
+    print(ab);
+  }
 }
