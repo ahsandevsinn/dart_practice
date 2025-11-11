4e2efd6d894feabefbde182de4370e0e5a55aa4d
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-11-11 08:57:18 +0500

    Arithmetic Operators

--- lib/20_days_dart/basics.dart
+++ lib/20_days_dart/basics.dart
@@ -1,18 +1,28 @@
 class Basics{
  static basic(){
-  var name = "Ahsan";
-  String fullName = "Ahsan Khan";
-  int age = 20;
-  double height = 4.0;
-  bool isFlutterDeveloper = true;
-  const pi = 3.12;
-  final dateTime = DateTime.now();
-  print(name);
-  print(fullName);
-  print(age);
-  print(height);
-  print(isFlutterDeveloper);
-  print(pi);
-  print(dateTime);
+  // var name = "Ahsan";
+  // String fullName = "Ahsan Khan";
+  // int age = 20;
+  // double height = 4.0;
+  // bool isFlutterDeveloper = true;
+  // const pi = 3.12;
+  // final dateTime = DateTime.now();
+  // print(name);
+  // print(fullName);
+  // print(age);
+  // print(height);
+  // print(isFlutterDeveloper);
+  // print(pi);
+  // print(dateTime);
+
+  // Arithmetic Operators
+  int a = 10; int b = 20;
+  print(a+b);
+  print(a-b);
+  print(a/b);
+  print(a%b);
+  print(a*b);
+  print(a~/b);
+  
   }
 }
