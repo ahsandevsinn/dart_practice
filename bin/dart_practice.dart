6de7fa85bab10f1fec3e6858e42332e224725dfb
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-12-28 01:59:45 +0500

    variables int double num

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,5 +1,6 @@
 import 'package:dart_practice/20_days_dart/basics.dart';
 import 'package:dart_practice/Operators/operators.dart';
+import 'package:dart_practice/basics/dart_basics.dart';
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
 import 'package:dart_practice/input/input.dart';
 import 'package:dart_practice/null_safety/null_safety.dart';
@@ -11,5 +12,5 @@ void main(List<String> arguments) {
   // PracticeOne.practiceOne();
   // Operators.operators();
   // PracticeTwo.practiceTwo();
-  Basics.basic();
+  DartBasics.dartBasics();
 }
---
+++ lib/basics/dart_basics.dart
@@ -0,0 +1,18 @@
+class DartBasics {
+  static dartBasics(){
+    // 1. What is a Variable?
+    // variable is a named of storage where user can store data 
+  
+  int age = 10;
+  double height = 5.5;
+  num personAge = 39;
+  num personHeight = 5.0;
+
+  print(age);
+  print(height);
+  print(personAge);
+  print(personHeight);
+
+  }
+  
+}
