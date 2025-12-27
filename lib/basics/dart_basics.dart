409ad2ba3644ba71cacecadb3be333dfd67a9c1e
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-12-28 02:06:34 +0500

    What is a variable? How is it different from a constant?

--- lib/basics/dart_basics.dart
+++ lib/basics/dart_basics.dart
@@ -3,15 +3,19 @@ class DartBasics {
     // 1. What is a Variable?
     // variable is a named of storage where user can store data 
   
-  int age = 10;
-  double height = 5.5;
-  num personAge = 39;
-  num personHeight = 5.0;
+  // int age = 10;
+  // double height = 5.5;
+  // num personAge = 39;
+  // num personHeight = 5.0;
 
-  print(age);
-  print(height);
-  print(personAge);
-  print(personHeight);
+  // print(age);
+  // print(height);
+  // print(personAge);
+  // print(personHeight);
+
+
+  // What is a variable? How is it different from a constant?
+  // variable is a named of storage where user can store data in constant value fixed but we can change variable value 
 
   }
   
