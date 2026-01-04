afd341ed414efee80ccaef261e8d3f25b52da9f3
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2026-01-04 17:58:28 +0500

    4️⃣ Question 4

--- lib/type_checking/type_checking.dart
+++ lib/type_checking/type_checking.dart
@@ -19,30 +19,37 @@ class TypeChecking {
 // int num1 = 32;
 // print(num1 as num);
 
-Object value = "Flutter";
-    // Object value = 7;
-    if (value is String) {
-      String text = value as String;
-      print(text);
-    } else {
-      print(value);
-    }
+// Object value = "Flutter";
+//     // Object value = 7;
+//     if (value is String) {
+//       String text = value as String;
+//       print(text);
+//     } else {
+//       print(value);
+//     }
 // dynamic numValue = 50;
 // double converted = numValue as double;
 // print(converted);
 // Q: Output kya hoga? Runtime error hoga ya nahi?
 // runtime error
 
-Object data = 100;
-if (data is String) {
-  String text = data as String;
-  print(text);
-} else {
-  print("Not a String");
+// Object data = 100;
+// if (data is String) {
+//   String text = data as String;
+//   print(text);
+// } else {
+//   print("Not a String");
 
-}
+// }
 // Q: Output kya hoga?
 // Not a String
 
+// 4️⃣ Question 4
+Object value = "Dart";
+if (value is String) {
+  print(value.length);
+}
+// Q: Kya value as String likhna zaroori hai?
+// iska answer length ayi gi 4 length
   }
 }
