703fa3672f3748ac6c47c64e342e4a45a8e04efd
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2026-01-04 17:14:02 +0500

    bitwise

--- lib/Operators/operators.dart
+++ lib/Operators/operators.dart
@@ -5,25 +5,32 @@ class Operators {
     // Arithemetic Operators
     int a = 5;
     int b = 10;
-    print(a +b);
-    print(a - b);
-    print(a * b);
-    print(a / b);
-    print(a % b);
+    // print(a +b);
+    // print(a - b);
+    // print(a * b);
+    // print(a / b);
+    // print(a % b);
     
 
-    // Relational Operator
-    print(a>b);
-    print(a<b);
-    print(a==b);
-    print(a!=b);
-    print(a>=b);
-    print(a<=b);
+    // // Relational Operator
+    // print(a>b);
+    // print(a<b);
+    // print(a==b);
+    // print(a!=b);
+    // print(a>=b);
+    // print(a<=b);
 
-    // Logical Operator
-    print(a > b && a < b);
-    print(a > b || b > a);
-    print(!(a>b));
+    // // Logical Operator
+    // print(a > b && a < b);
+    // print(a > b || b > a);
+    // print(!(a>b));
+
+    // Bitwise Operator
+    print(a.toRadixString(2));
+    print(b.toRadixString(2));
+    print(a & b);
+    print(a | b);
+    print(a^b);
 
 
   }
