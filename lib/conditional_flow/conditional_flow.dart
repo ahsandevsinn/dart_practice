3f03e566760f515dcd3dd03ba7fdb5d0416becce
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2026-01-05 09:42:39 +0500

    Ternary Operator

--- lib/conditional_flow/conditional_flow.dart
+++ lib/conditional_flow/conditional_flow.dart
@@ -7,35 +7,37 @@ class ConditionalFlow {
     //   print("Kid");
     // }
 
-  int day = 20;
-  switch (day){
-    case 1:
-    print("Monday");
-    break;
-    case 2:
-    print("Tuesday");
-    break;
-    case 3:
-    print("Wednesday");
-    break;
-    case 4:
-    print("Thursday");
-    break;
-    case 5:
-    print("Friday");
-    break;
-    case 6 :
-    print("Saturday");
-    break;
-    case 7 :
-    print("Sunday");
-    break;
+  // int day = 20;
+  // switch (day){
+  //   case 1:
+  //   print("Monday");
+  //   break;
+  //   case 2:
+  //   print("Tuesday");
+  //   break;
+  //   case 3:
+  //   print("Wednesday");
+  //   break;
+  //   case 4:
+  //   print("Thursday");
+  //   break;
+  //   case 5:
+  //   print("Friday");
+  //   break;
+  //   case 6 :
+  //   print("Saturday");
+  //   break;
+  //   case 7 :
+  //   print("Sunday");
+  //   break;
     
-    default:
-    print("Invalid day");
+  //   default:
+  //   print("Invalid day");
     
-  }
+  // }
 
+int age  = 19;
+print(age >= 18 ? "Adult" : "Kid");
   }  
 
 
