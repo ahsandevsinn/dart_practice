f0e164208e3d194417d5fe86db7a5d830ac4979c
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2026-01-07 09:48:19 +0500

    counting 0 to 100

--- lib/loops/loops.dart
+++ lib/loops/loops.dart
@@ -1,7 +1,13 @@
 class Loops {
   static loop(){
-    for(int i = 0;i < 10; i++){
-      print("HEllo");
+    // for(int i = 0;i < 10; i++){
+    //   print("HEllo");
+    // }
+
+    // counting 0 to 100
+    for(int i =0;i <= 100;i++){
+      print(i);
     }
   }
+  
 }
