ac82f851fdf1b98529d9472704c9bc5a3ddd2428
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2026-01-17 19:07:54 +0500

    dynamic or object

--- lib/variables/variables.dart
+++ lib/variables/variables.dart
@@ -7,6 +7,14 @@ class Variables {
     age = true;
     print(age);
     //object data type may bhi multiple values store hosakti hain same variable ki agansit but ye safe nai hai or production or modern day ki hisab say bhi theak nai hi iski wajha say app crashes atay hain isliye original data types use karni chiye
+    // object compile time per hi check karta hai 
     print(name);
+
+    dynamic username = "Ahsan";
+    username = 3;
+    username = 4.5;
+    username = true;
+    print(username * 6);
+    //dynamic compile time ko ignore karki run time per erro throw karta hai hai ager same variable may multiple data types ka data add ho or uski base per koi action perform karna ho to run time error ayi ga compile time nao
   }
 }
